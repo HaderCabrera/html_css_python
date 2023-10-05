be1afa1b88ce74c8e3179dfd62cab7f372172b92
HaderCabrera <hader.cabrera96@gmail.com>
2023-10-05 11:12:20 -0500

    avance taller

--- Estructura de datos/diccionarios/estudiantes.py
+++ Estructura de datos/diccionarios/estudiantes.py
@@ -1,16 +1,18 @@
 dicEscuela = {}
 bandera = True
-while True:
+while bandera == True:
     dicEstudiantes = {}
     if bandera == True:
-        Codigo = int("Ingrese el codigo del estudiante: ")
-        Nombre = input("Ingrese el nombre del estudiante: ")
-        Note1 = float(input("Ingrese la nota 1 del estudiante [0 - 5.0]"))
-        Note2 = float(input("Ingrese la nota 2 del estudiante [0 - 5.0]"))
-        Note3 = float(input("Ingrese la nota 3 del estudiante [0 - 5.0]"))
-        if Codigo == 999:
-            bandera == False
-    break
-
+        codigo = int(input("Ingrese el codigo del estudiante: "))
+        if codigo == 999:
+            break
+        else:
+            dicEstudiantes["Nombre"] = input("Ingrese el nombre del estudiante: ")
+            dicEstudiantes["Note1"] = float(input("Ingrese la nota 1 del estudiante [0 - 5.0]"))
+            dicEstudiantes["Note2"] = float(input("Ingrese la nota 2 del estudiante [0 - 5.0]"))
+            dicEstudiantes["Note3"] = float(input("Ingrese la nota 3 del estudiante [0 - 5.0]"))
+        
+        dicEscuela[codigo] = dicEstudiantes
+print(dicEscuela)
     
 
