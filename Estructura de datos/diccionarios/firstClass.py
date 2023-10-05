68c469c9717a1511e091fd03895bff61d46bce6d
HaderCabrera <hader.cabrera96@gmail.com>
2023-10-04 21:05:07 -0500

    cargar primer ejercicio diccionario dentro de dic

--- Estructura de datos/diccionarios/firstClass.py
+++ Estructura de datos/diccionarios/firstClass.py
@@ -3,13 +3,16 @@ empleado = {"nombre": "Hader Cabrera" , "Cargo": "Programador","Salario": 400000
 print(empleado["Cargo"])
 print(empleado.get("Cargo"))
 
-#PONER MENSAJE CUANDO NO EXISTE LLAVE
+#PONER MENSAJE CUANDO NO EXISTE LLAVE // me devuelve valor
 print(empleado.get("Apellido","NO EXITE")) #DEVUELVE NONE POR DEFECTO
 #print(empleado("Apellido")) #DEVUELVE ERROR
 
 #AGREGAR LLAVE A DICCIONARIO
 empleado["sexo"] = "Masculino"
 print(empleado)
+    #agregar con set: si no exite la crea, si exite devuelve el 
+    #valor original e imprimie para ambos casos
+print(empleado.setdefault("Localidad","Bucaramanga"))
 
 #MoODIFICAR SALAROIO
 empleado["Salario"] = 4500000
@@ -30,3 +33,20 @@ print(oficina)
 oficina["salario"] = 5000000
 print(oficina)
 print(empleado)
+
+#FROMKEYS
+
+#ITEMS: COMBIERTE LLAVE Y VALOR DE LLAVE EN UNA TUPLA DENTRO DE
+        #UNA LISTA
+print(empleado.items())
+
+#KEYS DEVUELVE LISTADO DE TODAS LAS LLAVES
+print(empleado.keys())
+
+#VALUE DEVUELVE LISTADO CON LOS VALORES EL DICCIONARIO
+print(empleado.values())
+
+#pop ELIMINA LLAVE Y VALOR DE LLAVE pero devuelve el VALOR
+print(empleado.pop("salario"))
+#ELIMINAR ULTIMA LLAVE Y VALOR
+print(empleado.popitem())
