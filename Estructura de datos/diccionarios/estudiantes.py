f0ec02179fb5b38d47162652080d433469f5063c
HaderCabrera <hader.cabrera96@gmail.com>
2023-10-05 18:04:37 -0500

    termine ejercicio estudiantes

--- Estructura de datos/diccionarios/estudiantes.py
+++ Estructura de datos/diccionarios/estudiantes.py
@@ -13,6 +13,21 @@ while bandera == True:
             dicEstudiantes["Note3"] = float(input("Ingrese la nota 3 del estudiante [0 - 5.0]"))
         
         dicEscuela[codigo] = dicEstudiantes
-print(dicEscuela)
+
+#CALCULAR NOTA DEFINITIVA
+def calPromedio(nota1,nota2,nota3):
+    promedio = (nota1 * 0.3) + (nota2 * 0.3) + (nota3 * 0.4)
+    return promedio
+
+#recorrer lista de tuplas 
+for c, d in dicEscuela.items():
+    nota1 = d["Note1"]  
+    nota2 = d["Note2"]
+    nota3 = d["Note3"]
+    nota = calPromedio(nota1,nota2,nota3)
+    if nota >= 3:
+        print(f'El estudiante {d["Nombre"]} [{c}] aprobo con una nota de {nota:.2f}')
+    else:
+        print(f'El estudiante {d["Nombre"]} [{c}] reprobo la materia con una nota de {nota:.2f}')
     
 
