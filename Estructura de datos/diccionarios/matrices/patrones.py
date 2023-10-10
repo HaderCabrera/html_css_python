f41909d61e1b85f364eecc270fc7dadeda3484da
HaderCabrera <hader.cabrera96@gmail.com>
2023-10-10 18:28:40 -0500

    ok

--- Estructura de datos/diccionarios/matrices/patrones.py
+++ Estructura de datos/diccionarios/matrices/patrones.py
@@ -17,55 +17,58 @@ def imprimirMatrix(matrix):
     print()
 def menu(smj):
   print("PATRONES DE MATRICES M*N DONDE M = N")
-  print("1. PATRON 1")
-  print("2. PATRON 2")
-  print("3. PATRON 3")
-  print("4. PATRON 4")
-  print("5. PATRON 5")
-  print("6. PATRON 6")
-  print("7. PATRON 7")
-  print("8. CAMBIAR DE MATRIX")
+  print("1. PATRON 1\t\t2. PATRON 2")
+  print("3. PATRON 3\t\t4. PATRON 4")
+  print("5. PATRON 5\t\t6. PATRON 6")
+  print("7. PATRON 7\t\t8. PATRON 8")
+  print("9. Cambiar tamaño de matrix.")
+  print("10. Salir.")
   op = int(input(smj))
   return op
   
 filas = int(input("Ingrese numero de filas:  "))
 matrix = crearMatrix(filas,filas)
 while True:
-  op = menu("   Indique que patron desea>>> ")
-  if op == 1:
-      contador = 1
-      for f in range(len(matrix)):
-        for c in range(len(matrix[f])):
-          matrix[f][c] = contador
-          contador += 1
-      imprimirMatrix(matrix)
-  elif op == 2:
-    numeros = []
-    bandera = 0
-    for k in range(1,filas+1):
-      numeros.append(k)
-    for f in range(len(matrix)):
-      if bandera == 0:
-        for c in range(len(matrix[f])):
-          matrix[f][c] = numeros[c]
-          bandera = 1
-      else:
-        rev = numeros[::-1]
-        for c in range(len(matrix[f])):
-          matrix[f][c] = rev[c]
-          bandera = 0
-
-    imprimirMatrix(matrix)
-  elif op == 3:
-    pass
-  elif op == 4:
-    pass
-  elif op == 5:
-    pass
-  elif op == 6:
-    pass
-  elif op == 7:
-    pass
-  elif op == 8:
-    filas = int(input("Ingrese numero de filas:  "))
-    matrix = crearMatrix(filas,filas)
+    op = menu("   Indique que patron desea>>> ")
+    if op == 1:
+        contador = 1
+        for f in range(len(matrix)):
+            for c in range(len(matrix[f])):
+                matrix[f][c] = contador
+                contador += 1
+        imprimirMatrix(matrix)
+        input()
+    elif op == 2:
+        numeros = []
+        bandera = 0
+        for k in range(1,filas+1):
+            numeros.append(k)
+        for f in range(len(matrix)):
+            if bandera == 0:
+                for c in range(len(matrix[f])):
+                    matrix[f][c] = numeros[c]
+                    bandera = 1
+            else:
+                rev = numeros[::-1]
+                for c in range(len(matrix[f])):
+                    matrix[f][c] = rev[c]
+                    bandera = 0
+        imprimirMatrix(matrix)
+        input()
+    elif op == 3:
+        pass
+    elif op == 4:
+        pass
+    elif op == 5:
+        pass
+    elif op == 6:
+        pass
+    elif op == 7:
+        pass
+    elif op == 8:
+        pass
+    elif op == 9:
+                filas = int(input("Ingrese numero de filas:  "))
+        matrix = crearMatrix(filas,filas)
+    elif op == 10:
+       break
