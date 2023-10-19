230984871e323ed78a8fdfe3bfd86297ab6f2659
HaderCabrera <hader.cabrera96@gmail.com>
2023-10-18 21:36:54 -0500

    avance proyecto

--- talleres/pruebas.py
+++ talleres/pruebas.py
@@ -1,32 +1,13 @@
+matDatos = [
+    [ "O", "X", "0"],
+    ["0" , "X", "0"],
+    ["O" , "O", "X"]
+]
 
-import json
-rutaFile = "hader_cabrera/talleres/datpersonal.json"
-lista = ["hola","como","esta"]
-while True:
-    try:
-        fd = open(rutaFile, "r")
-        print("Entre a lectura")
-    except Exception as e:
-        try:
-            fd = open(rutaFile, "w")
-            print("entre a escributra")
-        except Exception as d:
-            print("Error al intentar abrir el archivo\n", d)
-            input("Presione cualquier tecla para continuar\n")
-            break
-"""  try:
-    json.dump("hola",fd)
-except Exception as e:
-    print("Error al cargar la información\n", e)
-    input("Presione cualquier tecla para continuar\n")
-    break
-
-# print(lstPersonal)
-try:
-    if not fd.closed:
-        fd.close()
-except Exception as e:
-    print("Error al cerrar el archivo.\n", e, "\n")
-    input("Presione cualquier tecla para continuar\n")
-    break
-"""
+bandera = 0
+for f in range(len(matDatos)):
+    if matDatos[f][0] == matDatos[f][1] and matDatos[f][0] == matDatos[f][2]:
+        print("HAY GANADOR")
+    if matDatos[0][f] == matDatos[1][f] and matDatos[0][f] == matDatos[2][f]:
+        print("HAY GANADOR")       
+      
